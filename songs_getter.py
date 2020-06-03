82daaf23230df1a85aeed1da752b3838f15ef5cd
bartek-s13 <43931596+bartek-s13@users.noreply.github.com>
2020-06-03 12:26:32 +0200

    Add finding valid playlists
    add finding valid playlists based on keywords and playlists names
    add requirements.txt

--- songs_getter.py
+++ songs_getter.py
@@ -3,13 +3,20 @@
 
 import spotipy
 from spotipy import util
-import pandas as pd
 import yaml
+from typing import List
+import re
 
 
-def get_token(credentials_file_path:str, scp:str = None) -> str:
+def get_token(credentials_file_path: str, scp: str = None) -> str:
+    '''
+    Get Spotify user token
+    :param credentials_file_path: path to yaml file with credentials
+    :param scp: Spotify Authorization Scopes, by default scopes from file
+    :return: Spotify API token
+    '''
     with open(credentials_file_path) as file:
-        credentials= yaml.load(file, Loader=yaml.FullLoader)
+        credentials = yaml.load(file, Loader=yaml.FullLoader)
         username = credentials['username']
         client_id = credentials['client_id']
         client_secret = credentials['client_secret']
@@ -18,7 +25,42 @@ def get_token(credentials_file_path:str, scp:str = None) -> str:
     return util.prompt_for_user_token(username, scope, client_id, client_secret, redirect_uri)
 
 
-if __name__ == '__main__':
+def get_playlists(playlists:List, keywords:List) -> List:
+    '''
+    Get playlists whose name contains any of the keywords
+    :param playlists: 'items' from paging object with playlists returned by Spotify
+    :param keywords: list of words, playlist names should contain at least one of them to be returned
+    :return: list if tuples (playlist_id, playlist_name) of valid playlists
+    '''
+    keywords = ['.*'+word+'.*' for word in keywords]
+    valid_playlists = []
+    words = '|'.join(keywords)
+    print(words)
+    pattern = re.compile(words)
+    for playlist in playlists:
+        if pattern.match(playlist['name'].lower()):
+            valid_playlists.append((playlist['id'],playlist['name']))
+    return valid_playlists
 
+
+def get_user(credentials_file_path: str) -> str:
+    '''
+    get spotify user id
+    :param credentials_file_path: path to yaml file with credentials
+    :return: spotify user id
+    '''
+    with open(credentials_file_path) as file:
+        credentials= yaml.load(file, Loader=yaml.FullLoader)
+        return credentials['username']
+
+
+if __name__ == '__main__':
+    user = get_user('credentials.yaml')
     token = get_token('credentials.yaml')
     sp = spotipy.Spotify(auth=token)
+    my_playlists = sp.user_playlists(user)
+    rap_playlists = get_playlists(my_playlists['items'], ['rap', 'hip'])
+    print(rap_playlists)
+
+
+
