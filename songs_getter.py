816c4f80eb725caf60f235d2634515139571f744
Eryk <eryk.szpotanski@student.put.poznan.pl>
2020-06-05 01:34:51 +0200

    nothing changed

--- songs_getter.py
+++ songs_getter.py
@@ -1,5 +1,5 @@
-#https://blog.prokulski.science/index.php/2020/03/24/muzyka-i-data-science/
-#https://spotipy.readthedocs.io/en/2.9.0/#
+# https://blog.prokulski.science/index.php/2020/03/24/muzyka-i-data-science/
+# https://spotipy.readthedocs.io/en/2.9.0/#
 
 import spotipy
 from spotipy import util
@@ -28,21 +28,22 @@ def get_token(credentials_file_path: str, scp: str = None) -> str:
     return util.prompt_for_user_token(username, scope, client_id, client_secret, redirect_uri)
 
 
-def get_playlists(playlists:List, keywords:List) -> List:
+def get_playlists(playlists: List, keywords: List) -> List:
     '''
     Get playlists whose name contains any of the keywords
     :param playlists: 'items' from paging object with playlists returned by Spotify
     :param keywords: list of words, playlist names should contain at least one of them to be returned
     :return: list if tuples (playlist_id, playlist_name) of valid playlists
     '''
-    keywords = ['.*'+word+'.*' for word in keywords]
+    keywords = ['.*' + word + '.*' for word in keywords]
     valid_playlists = []
     words = '|'.join(keywords)
     pattern = re.compile(words)
     for playlist in playlists:
         if pattern.match(playlist['name'].lower()):
-            valid_playlists.append((playlist['id'],playlist['name']))
+            valid_playlists.append((playlist['id'], playlist['name']))
     return valid_playlists
+
 
 def get_playlist_songs(playlist_id, sp):
     playlist = sp.playlist(playlist_id)
@@ -58,7 +59,7 @@ def get_playlist_songs(playlist_id, sp):
     return tracks
 
 
-def get_genre_songs(playlists:List, sp):
+def get_genre_songs(playlists: List, sp):
     tracks = []
     for playlist in playlists:
         playlist_tracks = get_playlist_songs(playlist[0], sp)
@@ -66,8 +67,8 @@ def get_genre_songs(playlists:List, sp):
     return tracks
 
 
-def save_tracks(file_name: str, tracks:List):
-    path = Path("data/titles/"+file_name+'.csv')
+def save_tracks(file_name: str, tracks: List):
+    path = Path("data/titles/" + file_name + '.csv')
     tracks_df = pd.DataFrame(tracks, columns=['id', 'name', 'artist', 'album'])
     tracks_df.drop_duplicates(subset='id', inplace=True)
     tracks_df.to_csv(path, header=True, index=False)
@@ -81,21 +82,17 @@ def get_user(credentials_file_path: str) -> str:
     :return: spotify user id
     '''
     with open(credentials_file_path) as file:
-        credentials= yaml.load(file, Loader=yaml.FullLoader)
+        credentials = yaml.load(file, Loader=yaml.FullLoader)
         return credentials['username']
 
 
 if __name__ == '__main__':
-    #input arguments genre, *keywords
+    # input arguments genre, *keywords
     user = get_user('credentials.yaml')
     token = get_token('credentials.yaml')
     sp = spotipy.Spotify(auth=token)
     my_playlists = sp.user_playlists(user)
 
     valid_playlists = get_playlists(my_playlists['items'], sys.argv[2:])
-    tracks = get_genre_songs(valid_playlists,sp)
+    tracks = get_genre_songs(valid_playlists, sp)
     save_tracks(sys.argv[1], tracks)
-
-
-
-
