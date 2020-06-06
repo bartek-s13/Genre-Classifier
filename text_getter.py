9c7fe8891b060b42c2adb545186a1e382d8717a7
bartek-s13 <43931596+bartek-s13@users.noreply.github.com>
2020-06-06 14:03:32 +0200

    add downloading songs

--- text_getter.py
+++ text_getter.py
@@ -1,3 +1,5 @@
+import logging
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s')
 import pandas as pd
 import re
 from bs4 import BeautifulSoup
@@ -5,6 +7,9 @@ import requests
 import spacy
 from spacy_langdetect import LanguageDetector
 
+import os
+import sys
+import lyricsgenius
 
 def request_song_info(song_title, artist_name):
     base_url = 'https://api.genius.com'
@@ -85,6 +90,41 @@ def check_if_english(lyrics: str) -> bool:
     return doc._.language['language'] == 'en'
 
 
+def get_top_artists(filename):
+    data = pd.read_csv("data/titles/" + filename + ".csv")
+    count = data.groupby("artist")["name"].count().reset_index(name='count').sort_values(['count'], ascending=False)
+    return count.artist.values
+
+
+def lyrics_downloader(filename, songs_per_artist=6, songs_num=3000):
+    genius = lyricsgenius.Genius("9y4ky-47cpjAgFmIljdSVhT306jZsIVF4NfQ43eTQ-cjd3yXrzTAdxjkttCUV0SN")
+    genius.verbose = False
+    genius.remove_section_headers = True
+
+    already_downloaded = 0
+    artists = get_top_artists(filename)
+    os.chdir('./data/lyrics/' + filename)
+    for artist in artists:
+        try:
+            art = genius.search_artist(artist, max_songs=songs_per_artist)
+            for song in art.songs:
+                try:
+                    title = '_'.join(song.title.split())
+                    song.to_text(filename=title+".txt")
+                    logging.info(f"{song.title} saved")
+                    already_downloaded+=1
+                    logging.info(f"{already_downloaded} out of {songs_num} downloaded")
+                    if  already_downloaded == songs_num:
+                        break
+                except:
+                    logging.error(f"{song.title} not saved")
+            if already_downloaded == songs_num:
+                break
+        except:
+            logging.ERROR("Artist: {} was not found".format(artist.name))
+
+
+
 
 if __name__ == "__main__":
     # get_songs("rap_hip_hop")
@@ -92,14 +132,23 @@ if __name__ == "__main__":
     # for a in get_songs("rap_hip_hop"):
     #     tekstowo_artist(a)
     # get_lyrics("Ms. Jackson", "OutKast")
-    songs = get_songs("rap_hip_hop")
-    for z in songs:
+    #songs = get_songs("rap_hip_hop")
+    #for z in songs:
     #     print(str(z[0]) + '\t' + str(z[1]))
-        lyric = normalize_lyric(get_lyrics(str(z[1]), str(z[0])))
-        if lyric is not None:
-            print(lyric)
-        break
+        #lyric = normalize_lyric(get_lyrics(str(z[1]), str(z[0])))
+        #if lyric is not None:
+            #print(lyric)
+        #break
         # lyric = scrapping_lyrics("Since u been gone", "Kelly Clarkson")
         # if lyric:
         #     print(lyric)
         # break
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    import sys
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(logging.INFO)
+    logger.addHandler(ch)
+    #for filename in os.listdir("data/titles/"):
+        #lyrics_downloader(filename)
+    lyrics_downloader('rock_metal')
